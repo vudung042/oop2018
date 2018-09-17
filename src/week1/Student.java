76a4c332712b5ed6cd33c32ab30291a43683bd9b
Duc Dung <vudung042@gmail.com>
2018-09-18 00:10:01 +0700

    add Student

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,42 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
+    private String name, id, group, email;
 
     // TODO: khai báo các phương thức getter, setter cho Student
-
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setGroup(String group){
+        this.group = group;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        System.out.println("Student");
+        System.out.println("000");
+        System.out.println("INT22041");
+        System.out.println("uet@vnu.edu.vn");
     }
 
     /**
@@ -31,8 +59,15 @@ public class Student {
         // TODO:
     }
 
-    String getInfo() {
+    void getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println("Name: " + name);
+
+        System.out.println("Id: " + id);
+
+        System.out.println("Group: " + group);
+
+        System.out.println("Email: " + email);
+
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -4,6 +4,7 @@ public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
 
+
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
         return false; // xóa dòng này sau khi cài đặt
@@ -19,5 +20,14 @@ public class StudentManagement {
 
     public static void main(String[] args) {
         // TODO:
+        Student std = new Student();
+        Student std1 = new Student();
+        std1.setName("Duc Dung");
+        std1.setId("17020643");
+        std1.setGroup("INT2204 8");
+        std1.setEmail("vudung042@gmail.com");
+
+       System.out.println(std1.getName());
+       std1.getInfo();
     }
 }
