953054006ca6d86d5d017d3f7fc1399a06706fd6
Duc Dung <vudung042@gmail.com>
2018-11-07 16:23:44 +0700

    update week9

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class Utils {
     public static String readContentFromFile(String path) {
-        File file = new File(path);
+
         try {
+            File file = new File(path);
             BufferedReader br = new BufferedReader(new FileReader(file));
-//            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
-            br.readLine();
-            String line = "";
+
+            String line ;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
@@ -25,17 +25,18 @@ public class Utils {
         System.out.println("Nhập xâu cần thêm: ");
         Scanner input = new Scanner(System.in);
         String find_word = input.nextLine();
-        File f = new File(path);
+
         try {
-            FileWriter fw = new FileWriter(f);
-//            BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "UTF8"));
-            fw.write("\r\n");
+            File f = new File(path);
+            BufferedWriter fw = new BufferedWriter(new FileWriter(f));
             fw.write(find_word);
+            fw.write("\r\n");
             fw.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
+
     // cau c chi can them true FileWriter(f,true)//
 
     public static File findFileByName(String folderPath, String fileName){
