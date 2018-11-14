475ffaaaef7ea6e8f20e7383da1503c08af9f1c7
Duc Dung <vudung042@gmail.com>
2018-11-14 13:30:17 +0700

    update week10

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -8,13 +8,13 @@ public class Task1 {
     public List<String> getAllFunction(String path){
 
          List<String> a = new ArrayList<String>();
-            String line = null;
+            String line = "";
 
             try {
                 File file = new File(path);
                 BufferedReader br = new BufferedReader(new FileReader(file));
                 while((line=br.readLine())!=null) {
-                if(line.indexOf("static")>0) {
+                if(line.contains("static")) {
                 a.add(line);
             }
         }
@@ -24,11 +24,35 @@ public class Task1 {
     }
     return a;
 }
+
+    public String findFunctionByName(String name)  {
+        String line = "";
+        Boolean check = false;
+        try {
+            File file = new File("C:\\Users\\dell\\IdeaProjects\\oop20182\\src\\week9\\Utils.java");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            while((line=br.readLine())!=null) {
+                if(line.contains(name)) {
+                check = true;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(check){
+            return line;
+        }
+        else return "Function didn't found";
+    }
+
     public static void main(String[] args) {
         Task1 task1 = new Task1();
         ArrayList<String> method1 = (ArrayList<String>) task1.getAllFunction("C:\\Users\\dell\\IdeaProjects\\oop20182\\src\\week9\\Utils.java");
         for (int i = 0; i < method1.size(); i++) {
             System.out.println(method1.get(i));
         }
+        System.out.println(task1.findFunctionByName("writeContentToFile(String)"));
     }
 }
