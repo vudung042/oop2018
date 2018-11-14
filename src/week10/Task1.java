e7371d989eadcd600b9306212d7af2bf1a827fb0
Duc Dung <vudung042@gmail.com>
2018-11-14 08:39:13 +0700

    update week10

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,34 @@
 package week10;
 
+import java.io.*;
+import java.util.*;
+
 public class Task1 {
+
+    public List<String> getAllFunction(String path){
+
+         List<String> a = new ArrayList<String>();
+            String line = null;
+
+            try {
+                File file = new File(path);
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                while((line=br.readLine())!=null) {
+                if(line.indexOf("static")>0) {
+                a.add(line);
+            }
+        }
+        br.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return a;
+}
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        ArrayList<String> method1 = (ArrayList<String>) task1.getAllFunction("C:\\Users\\dell\\IdeaProjects\\oop20182\\src\\week9\\Utils.java");
+        for (int i = 0; i < method1.size(); i++) {
+            System.out.println(method1.get(i));
+        }
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,31 @@
 package week10;
 
+import java.util.*;
+
 public class Task2 {
+    public void Swap() {
+        double[] a = new double[2000];
+        Random rd = new Random();
+        for(int i=0;i<1000;i++) {
+            a[i] = rd.nextDouble();
+        }
+        for(int i = 0; i<1000; i++) {
+            for (int j = i; j < 1000; j++) {
+
+                if (a[i] > a[j]) {
+                    double t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
+            }
+        }
+        for(int i=0;i<1000;i++) {
+            System.out.println(a[i]);
+        }
+    }
+    public static void main(String[] args){
+        Task2 task2=new Task2();
+        task2.Swap();
+    }
 }
+
