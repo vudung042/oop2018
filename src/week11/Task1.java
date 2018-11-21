6ef26bad0d41562566a1a64da90e254a799cc840
Duc Dung <vudung042@gmail.com>
2018-11-21 20:18:15 +0700

    update week11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,25 @@
 package week11;
 
-public class Task1 {
-}
+
+    public class Task1 {
+            public static <E extends Comparable> void sort(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i].compareTo(arr[j]) > 0) {
+                    E temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] Arr = {4, 1, 9, 3, 12, 20};
+        sort(Arr);
+        for (int i = 0; i < Arr.length; i++) {
+            System.out.println(Arr[i]);
+        }
+    }
+    }
+
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,28 @@
 package week11;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Task2 {
+public static <E extends Comparable> E GetMax(ArrayList<E> arr)
+{
+    E max = arr.get(0);
+    for(int i = 1 ; i < arr.size() ; i++) {
+        if(arr.get(i).compareTo(max) > 0)
+        {
+            max = arr.get(i);
+        }
+    }
+    return max;
+}
+    public static void main(String[] args) {
+        ArrayList<Integer> Arr=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        for(int i=0;i<num;i++)
+        {
+            Arr.add(scanner.nextInt());
+        }
+        System.out.println(GetMax(Arr));
+    }
 }
